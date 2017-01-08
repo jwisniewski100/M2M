8a0e6451b19be5bdf0989442aa25032246975833
Jarek Wiśniewski <jarek winiewski>
2017-01-08 18:06:31 +0100

    New login and register form

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,16 +1,37 @@
 function config($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/index/minor");
+    //$urlRouterProvider.otherwise("/index/minor");
+    $urlRouterProvider.otherwise("/login/minor");
 
     $stateProvider
 
+        .state('login', {
+            abstract: true,
+            url: "/login",
+            //templateUrl: "views/common/content.html",
+            templateUrl: "views/common/contentLogin.html",
+            //templateUrl: "login.html",
+        })
         .state('index', {
             abstract: true,
             url: "/index",
             templateUrl: "views/common/content.html",
+            //templateUrl: "login.html",
         })
 	    .state('index.minor', {
             url: "/minor",
             templateUrl: "views/minor.html",
+            //templateUrl: "login.html",
+            data: { pageTitle: 'Example view' }
+        })
+	    .state('login.minor', {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            //templateUrl: "login.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('login.register', {
+            url: "/register",
+            templateUrl: "views/register.html",
             data: { pageTitle: 'Example view' }
         })
         .state('index.main', {
