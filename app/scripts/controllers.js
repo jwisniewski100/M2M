4a0887178a4dd9fe143add03ac59a8fbced74544
bartlomiejek <baartekzib@gmail.com>
2017-01-29 16:03:48 +0100

    Add sim cards chart

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -39,35 +39,11 @@
 
 	this.setNewUrl(getCookie('url'));
 
-	$scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+	$scope.chartlabels = ['ClassicMini', 'ClassicMicro', 'A1Clasic', 'A1d23ultra', 'NanoInter' ];
 
-	$scope.labels1 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	$scope.chartdata = [5, 7, 0, 9, 3];
 	
-	$scope.data1 = [
-	    [65, -59, 80, 81, -56, 55, -40],
-	    [28, 48, -40, 19, 86, 27, 110]
-	  ];
-	$scope.datasetOverride1 = [
-	    {
-	      label: 'Override Series A',
-	      borderWidth: 1,
-	      type: 'bar'
-	    },
-	    {
-	      label: 'Override Series B',
-	      borderWidth: 3,
-	      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-	      hoverBorderColor: 'rgba(255,99,132,1)',
-	      type: 'line'
-	    }
-	  ];
-
-	  $scope.labels2 = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-	  $scope.data2 = [350, 450, 100];
-	  $scope.datasetOverride2 = {
-	    hoverBackgroundColor: ['#45b7cd', '#ff6384', '#ff8e72'],
-	    hoverBorderColor: ['#45b7cd', '#ff6384', '#ff8e72']
-	  };
+	
 	};
 
     $('#Order').submit(function(e) {
